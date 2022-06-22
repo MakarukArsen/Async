6bf7d1774bc8d2e340d72b80acfd58229b46508e
MakarukArsen <makarukarsen@gmail.com>
2022-06-22 20:10:43 +0300

    1.0

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("123")
+function getRandomChinese(length) {
+    const startLength = length;
+    const result = [];
+    const createChineseString = new Promise((res, rej) => {
+            setTimeout(function next() {
+                const sign = String(Date.now()).slice(-5);
+                const chineseSign = String.fromCharCode(sign);
+                result.push(chineseSign);
+                --length;
+                if(length > 0){
+                    setTimeout(next, 50);
+                }
+                if(!result.length){
+                    rej("Щось пішло не так :(");
+                }
+                if(result.length === startLength){
+                    res(result.join(""));
+                }
+            }, 50);
+    })
+    createChineseString.then((res) => {console.log(res)});
+    createChineseString.catch(rej => rej);
+    return createChineseString;
+}
+getRandomChinese(5);
